2035afef3749616ccb9e3ac3a4e7995a67b2df4c
RionPark <redfila@nate.com>
2018-06-30 08:49:02 +0900

    1

--- server/server.js
+++ server/server.js
@@ -4,16 +4,6 @@ const app = express();
 const conf = require('./conf/conf');
 app.use(express.static(__dirname + '/../dist/test-boot-app'));
 
-var chokidar = require('chokidar')
-var watcher = chokidar.watch('./server'); 
-watcher.on('ready', function() {
-  watcher.on('all', function() {
-    console.log("Clearing /dist/ module cache from server")
-    Object.keys(require.cache).forEach(function(id) { 
-        delete require.cache[id]
-    })
-  })
-})
 
 const uc = require('./controller/user_cont');
 
